93d4355367adc7753d47ee30a4c77544d332b51e
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-02-28 10:43:25 +0700

    menambahkan baseurl

--- config/functions.php
+++ config/functions.php
@@ -4,10 +4,12 @@ class functions {
 	public $conn;
 	public $breadcrumb;
 	public $title;
+	public $baseurl;
 
 	public function __construct()
 	{
 		$this->conn = mysqli_connect('localhost','root','','restoran');
+		$this->baseurl = "http://localhost/restoran/";
 	}
 
 	public function set_breadcrumb($list)
