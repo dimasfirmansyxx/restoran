e3433bece90b91c96b7feae04efe719a3f93a787
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-02-28 21:13:22 +0700

    modifikasi template untuk responsive

--- config/templates/header.php
+++ config/templates/header.php
@@ -4,6 +4,7 @@
 	<title><?= $myfunc->title ?></title>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<script src="assets/js/jquery.js"></script>
 	<script src="assets/js/popper.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
