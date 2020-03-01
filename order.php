ff970258db2987543985dff59448b0e0e119c5d7
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-03-01 09:15:20 +0700

    membuat fungsi menambah item untuk order

--- order.php
+++ order.php
@@ -11,14 +11,27 @@
 		$myfunc->redirect($myfunc->baseurl . "index.php");
 	}
 
+	if ( !isset($_SESSION["id_transaksi"]) ) {
+		$_SESSION["id_transaksi"] = $myfunc->get_id_transaksi();
+	}
 
 	$get_data = $myfunc->get_all_menu();
 	$get_cust = $myfunc->get_all_pelanggan();
+
+	if ( isset($_POST['cart']) ) {
+		if ( $_POST['qty'] == 0 ) {
+			$myfunc->notif("Qty harus lebih dari 1");
+			$myfunc->redirect($myfunc->baseurl . "order.php");
+		} elseif ( !(is_numeric($_POST['qty'])) ) {
+			$myfunc->notif("Isi qty hanya dengan angka");
+			$myfunc->redirect($myfunc->baseurl . "order.php");
+		}
+	}
 ?>
 
 <div class="row">
 	<div class="col-12 text-right mb-4">
-		<form class="form-inline">
+		<form action="" method="post" class="form-inline">
 			<select class="form-control mr-3" required name="pelanggan">
 				<option value="0">--- Pilih Pelanggan ---</option>
 				<?php foreach ($get_cust as $cust): ?>
@@ -29,7 +42,7 @@
 			<a href="#" class="btn btn-danger mr-3">Batal</a>
 		</form>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-5">
 		<div class="card">
 			<div class="card-header bg-dark text-white">
 				List Menu
@@ -39,8 +52,9 @@
 					<table class="table table-bordered">
 						<thead>
 							<tr>
-								<th>Nama</th>
+								<th>Menu</th>
 								<th>Harga</th>
+								<th width="100">Qty</th>
 								<th width="50">Aksi</th>
 							</tr>
 						</thead>
@@ -54,9 +68,56 @@
 									<tr>
 										<td><?= $row['nama_menu'] ?></td>
 										<td>Rp.<?= number_format($row['harga']) ?></td>
+										<form action="" method="post">
+											<td>
+												<input type="number" name="qty" required class="form-control">
+											</td>
+											<td>
+												<button type="submit" name="cart" value="<?= $row['id_menu'] ?>" class="btn btn-primary btn-sm">Pesan</a>
+											</td>
+										</form>
+									</tr>
+								<?php endforeach ?>
+							<?php endif ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-7">
+		<div class="card">
+			<div class="card-header bg-dark text-white">
+				List Pesanan
+			</div>
+			<div class="card-body">
+				<div class="table-responsive">
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+								<th width="50">#</th>
+								<th>Menu</th>
+								<th>Harga</th>
+								<th>Qty</th>
+								<th>Subtotal</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php if ( $get_data == 3 ): ?>
+								<tr>
+									<td colspan="5" class="text-center">Tidak ada menu</td>
+								</tr>
+							<?php else: ?>
+								<?php foreach ($get_data as $row): ?>
+									<tr>
 										<td>
-											<a href="<?= $myfunc->baseurl ?>order_pesan.php?id=<?= $row['id_menu'] ?>" class="btn btn-primary btn-sm">Pesan</a>
+											<a href="<?= $myfunc->baseurl ?>order.php?hapus=<?= $row['id_menu'] ?>" class="btn btn-danger btn-sm">x</a>
 										</td>
+										<td><?= $row['nama_menu'] ?></td>
+										<td>Rp.<?= number_format($row['harga']) ?></td>
+										<td>a</td>
+										<td>a</td>
 									</tr>
 								<?php endforeach ?>
 							<?php endif ?>
